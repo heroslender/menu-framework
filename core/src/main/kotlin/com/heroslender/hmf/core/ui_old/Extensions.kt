06a60714613769772209115fc6f05e4d1a51c876
heroslender <bruno.martins1998@hotmail.com>
2021-04-12 19:44:40 +0100

    Remove old component code

--- core/src/main/kotlin/com/heroslender/hmf/core/ui_old/Extensions.kt
+++
@@ -1,7 +0,0 @@
-package com.heroslender.hmf.core.ui_old
-
-import com.heroslender.hmf.core.IColor
-import com.heroslender.hmf.core.MutableStateImpl
-import com.heroslender.hmf.core.State
-
-typealias DrawFunc = (x: Int, y: Int, color: IColor) -> Unit
